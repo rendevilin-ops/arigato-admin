d2cf5ba4799dc81c99124a2e673965e8a65a7943
rendevilin-ops <rendevilin@gmail.com>
2025-12-10 14:44:02 +0100

    Update Loading.tsx

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,1 +1,7 @@
-// placeholder
+export default function Loading() {
+  return (
+    <div className="py-6 flex justify-center">
+      <div className="animate-spin rounded-full h-6 w-6 border-b-2 border-gray-500" />
+    </div>
+  );
+}
