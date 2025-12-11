77b7fec9b0ebb142d9e32321de303f3b17715874
rendevilin-ops <rendevilin@gmail.com>
2025-12-11 02:18:11 +0100

    Update page.tsx

--- app/admin/reservations/[id]/page.tsx
+++ app/admin/reservations/[id]/page.tsx
@@ -98,7 +98,7 @@ export default function ReservationDetailPage({ params }) {
           {(() => {
             const times = [
               "12:00","12:30",
-              "13:00","13:30",
+              "13:00","13:30","14:00",
               "19:00","19:30",
               "20:00","20:30",
               "21:00"
