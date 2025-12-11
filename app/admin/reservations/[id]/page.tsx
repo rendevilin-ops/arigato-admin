ef3f6610c47c6cc7d42ed15ddefcf76247dc76f8
rendevilin-ops <rendevilin@gmail.com>
2025-12-11 02:13:19 +0100

    Update page.tsx

--- app/admin/reservations/[id]/page.tsx
+++ app/admin/reservations/[id]/page.tsx
@@ -94,17 +94,34 @@ export default function ReservationDetailPage({ params }) {
         {/* ArrivalTime */}
         <div>
           <label className="block text-sm font-medium">Arrival Time</label>
-        <input
-          type="time"
-          step="900"
-          value={roundTo15(data.ArrivalTime)}
-          onChange={(e) =>
-            setData({ ...data, ArrivalTime: e.target.value })
-          }
-          className="border p-2 rounded w-full"
-        />
-          
+        
+          {(() => {
+            const times = [
+              "11:30","11:45","12:00","12:15","12:30","12:45",
+              "13:00","13:15","13:30","13:45",
+              "19:00","19:15","19:30","19:45",
+              "20:00","20:15","20:30","20:45",
+              "21:00","21:15","21:30"
+            ];
+        
+            return (
+              <select
+                value={data.ArrivalTime}
+                onChange={(e) =>
+                  setData({ ...data, ArrivalTime: e.target.value })
+                }
+                className="border p-2 rounded w-full"
+              >
+                {times.map((t) => (
+                  <option key={t} value={t}>
+                    {t}
+                  </option>
+                ))}
+              </select>
+            );
+          })()}
         </div>
+
 
         {/* First / Last Name */}
         <div>
